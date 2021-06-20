ad6d41d3d77279f8dab8e6a92a1007c11d95bd7e
Nazar Velychko <nazar.vel@gmail.com>
2021-06-20 15:30:12 +0300

    New Error tag and try catch in main js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,12 @@ import { NotesTableList, StatsTableList } from "./components/index.js";
 const NotesTableListInstance = new NotesTableList();
 const StatsTableListInstance = new StatsTableList();
 
-const errorElement = document.createElement("p")
-errorElement.textContent = "Error has been occurred"
-document.querySelector("body").appendChild(errorElement)
-
 try {
     NotesTableListInstance.render();
     StatsTableListInstance.render();
 }
 catch (e) {
-    const errorElement = document.createElement("p")
-    errorElement.textContent = "Error has been occurred"
-    document.appendChild(errorElement)
-
+    const errorMessage = document.createElement("error-message")
+    errorMessage.setAttribute("errorMessage", e.message)
+    document.querySelector(".page__container").prepend(errorMessage)
 }
--- js/tags/index.js
+++ js/tags/index.js
@@ -1,9 +1,11 @@
 import { NotesTableRow, NotesTableHeader} from "./NotesTable/index.js";
 import { StatsTableRow, StatsTableHeader } from "./StatsTable/index.js"
 import { CustomTable } from "./containers/index.js";
+import ErrorMessage from "./ErrorMessage/ErrorMessage.js";
 
 window.customElements.define('notes-table-header', NotesTableHeader)
 window.customElements.define('stats-table-header', StatsTableHeader)
 window.customElements.define('notes-table-row', NotesTableRow)
 window.customElements.define('stats-table-row', StatsTableRow)
 window.customElements.define('custom-table', CustomTable)
+window.customElements.define('error-message', ErrorMessage)
