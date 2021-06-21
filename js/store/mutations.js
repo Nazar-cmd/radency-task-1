285adb3422ba21f4d5517e6bd5c59b6a21352b4b
Nazar Velychko <nazar.vel@gmail.com>
2021-06-22 01:53:52 +0300

    remake mutations for not calling a few times

--- js/store/mutations.js
+++ js/store/mutations.js
@@ -1,17 +1,36 @@
 export default {
     createNote(state, payload) {
-        state.notes.push(payload.note)
-        
+        const copy = [...state.notes];
+        copy.push(payload.note)
+
+        state = {
+            ...state,
+            notes: copy
+        }
+
         return state;
     },
     deleteNote(state, payload) {
-        state.notes.splice(payload.index, 1)
+        const copy = [...state.notes];
+        copy.splice(payload.index, 1)
+
+        state = {
+            ...state,
+            notes: copy
+        }
 
         return state;
     },
     updateNote(state, payload) {
-        const {index, newNote} = payload
-        state.notes[index] = newNote;
+        const {index, newNote} = payload;
+        const copy = [...state.notes];
+
+        copy[index] = newNote;
+
+        state = {
+            ...state,
+            notes: copy
+        }
 
         return state;
     },
@@ -20,18 +39,30 @@ export default {
         const isArchived = copy[payload.index].archived
         copy[payload.index].archived = !isArchived;
 
+        state = {
+            ...state,
+            notes: copy
+        }
+
         return state
     },
     deleteAllNotes(state, payload) {
-        state.notes = []
+        state = {
+            ...state,
+            notes: []
+        }
 
         return state
     },
     archiveAllNotes(state, payload) {
-        state.notes = state.notes
-            .map(note => {
-                return {...note, archived: true}
-            })
+        const copy = state.notes.map(note => {
+            return {...note, archived: true}
+        })
+
+        state = {
+            ...state,
+            notes: copy
+        }
 
         return state
     },
