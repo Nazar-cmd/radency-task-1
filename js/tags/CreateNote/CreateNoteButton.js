25f2c763ff2bf03f98ef18f4dbc5c689e941e512
Nazar Velychko <nazar.vel@gmail.com>
2021-06-21 13:45:02 +0300

    added select

--- js/tags/CreateNote/CreateNoteButton.js
+++ js/tags/CreateNote/CreateNoteButton.js
@@ -3,17 +3,13 @@ const createNoteButtonTemplate = document.createElement("template");
 createNoteButtonTemplate.innerHTML = `
 <div class="add_note__container">
     <button type="button" class="add_note__button" >
-        <input type="checkbox" id="add_note__button_input" class="add_note__button_input"/>
-        <label for="add_note__button_input" class="add_note__button_text">
+        <span class="add_note__button_text">
             Add note!
-        </label>
-        <div class="add_note__popup__container">
-            <!--<label for="add_note__button_input" class="add_note__button_text">
-                X
-            </label>-->
-            <create-note-popup></create-note-popup>
-        </div>
+        </span>
     </button>
+     <div class="add_note__popup__container closed">
+        <create-note-popup></create-note-popup>
+     </div>
 </div>
 `
 
@@ -31,6 +27,10 @@ export default class CreateNoteButton extends HTMLElement{
 
         shadow.appendChild(createNoteButtonTemplate.content.cloneNode(true))
 
+        shadow.querySelector(".add_note__button")
+            .addEventListener("click", () => {
+                shadow.querySelector(".add_note__popup__container").className = "add_note__popup__container"
+        })
 
         /*const icon_delete_all = shadow.querySelector(".icon_delete_all")
         icon_delete_all.addEventListener("click", ()=>{
--- js/tags/CreateNote/CreateNotePopup.js
+++ js/tags/CreateNote/CreateNotePopup.js
@@ -2,15 +2,29 @@
 const createNotePopupTemplate = document.createElement("template");
 createNotePopupTemplate.innerHTML = `
 <div class="add_note__popup">
+    <div class="popup__exit">
+        <img class="popup__exit_icon" src="assets/icons/times-solid.svg" alt="close">
+    </div>
     <div class="popup__group">
-        <input type="text" id="note__name" class="popup__input" placeholder="Note name">
+        <input type="text" id="note__name" class="popup__input" placeholder="Note name" required>
         <label for="note__name" class="popup__label">Note name</label>
     </div>
-    <div class="popup__group">
-        <textarea id="note__content" class="popup__input" placeholder="Note content" rows="3"></textarea>
-        <label for="note__name" class="popup__label">Note content</label>
+    <div class="popup__group popup__input_select">
+        <select class="popup__input" required>
+            <option class="popup__input_variant" value="" disabled selected></option>
+            <option class="popup__input_variant" value="Random Thought">Random Thought</option>
+            <option class="popup__input_variant" value="Task">Task</option>
+            <option class="popup__input_variant" value="Idea">Idea</option>
+            <option class="popup__input_variant" value="Quote">Quote</option>
+        </select>
+        <span class="select-highlight"></span>
+        <label class="popup__label">Note name</label>
     </div>
-    <button type="button" class="popup__button">
+    <div class="popup__group">
+        <textarea id="note__content" class="popup__input popup__input_textarea" placeholder="Note content" rows="3" required></textarea>
+        <label for="note__content" class="popup__label">Note content</label>
+    </div>
+    <button type="button" class="popup__button popup__button_add">
         <h3>CREATE</h3>
     </button>
 </div>
@@ -20,22 +34,41 @@ export default class CreateNotePopup extends HTMLElement{
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
+        this.shadow = this.attachShadow({mode: "open"});
 
         const linkElem = document.createElement('link');
         linkElem.setAttribute('rel', 'stylesheet');
         linkElem.setAttribute('href', 'scss/components/createNotePopup.css');
 
-        shadow.appendChild(linkElem);
+        this.shadow.appendChild(linkElem);
 
-        shadow.appendChild(createNotePopupTemplate.content.cloneNode(true))
+        this.shadow.appendChild(createNotePopupTemplate.content.cloneNode(true))
 
-        //console.log(this.getRootNode().host.innerHTML)
+        this.shadow.querySelector(".popup__button_add").addEventListener("click", this.addNote)
+
+        this.shadow.querySelector(".popup__exit_icon").addEventListener("click", this.closePopup)
 
 
         /*const icon_delete_all = shadow.querySelector(".icon_delete_all")
         icon_delete_all.addEventListener("click", ()=>{
             store.dispatch("deleteAllNotes", {})
         })*/
+    }
+
+    closePopup = () => {
+        const popupContainer = this.shadow.host.getRootNode()
+            .childNodes[2].childNodes[3].parentElement
+            .querySelector(".add_note__popup__container");
+
+        popupContainer.className = "add_note__popup__container closed"
+    }
+
+    addNote = () => {
+        const name = this.shadow.querySelector("#note__name").value
+        const content = this.shadow.querySelector("#note__content").value
+
+        console.log({name, content})
+
+        this.closePopup()
     }
 }
